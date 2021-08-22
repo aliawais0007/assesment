04d4feb68601846b6a3680175db7c415134e18a9
aliawais0007 <ali.awais5044@gmail.com>
2021-08-22 15:58:40 +0400

    gh-pages

--- src/components/home/SuperMerchants.jsx
+++ src/components/home/SuperMerchants.jsx
@@ -1,8 +1,46 @@
+import { useState, useEffect, useRef } from "react";
 import Typed from "react-typed";
+import baloons from "../../assets/images/baloons.svg";
 
 export const SuperMerchants = () => {
+  const imgRef = useRef();
+  const [isIntersecting, setIntersecting] = useState(false);
+  const [transform, setTransform] = useState(0);
+
+  const observer = new IntersectionObserver(([entry]) =>
+    setIntersecting(entry.isIntersecting)
+  );
+
+  const topPosition =
+    imgRef.current && imgRef.current.getBoundingClientRect().top;
+  const onScroll = () => {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    
+    if (isIntersecting) {
+      setTransform(transform + 50);
+      imgRef.current.style.transform = `rotateX(${transform + 50}px`;
+    }
+  };
+  useEffect(() => {
+    observer.observe(imgRef.current);
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+      observer.disconnect();
+    };
+    if (isIntersecting) {
+    }
+  }, [isIntersecting]);
   return (
     <section id="merchants">
+      <div className={"bg-wrapper"}>
+        <img
+          src={baloons}
+          alt=""
+          ref={imgRef}
+          style={{ transform: "rotate(0px, 0)" }}
+        />
+      </div>
       <div className="container-fluid position-relative h-100">
         <div className="row justify-content-center h-100">
           <div className="col-12 col-md-10 col-lg-6 d-flex flex-column justify-content-center">
