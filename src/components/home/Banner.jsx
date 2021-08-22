d69e5d889b73c36d53e20fe22899f18b8d3b3f3f
aliawais0007 <ali.awais5044@gmail.com>
2021-08-22 17:52:46 +0400

    btn position fixed

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -7,7 +7,9 @@ export const Banner = () => {
       <div className={"container-fluid"}>
         <div className={"row justify-content-center "}>
           <div
-            className={"col-12 col-md-10 col-lg-6 p-40 d-flex align-items-end"}
+            className={
+              "col-12 col-md-10 col-lg-6 p-40 d-flex align-items-end order-lg-1 order-2"
+            }
           >
             <div className={"wrapperCol"}>
               <h2 className={"larger-heading banner-heading"}>
@@ -15,15 +17,29 @@ export const Banner = () => {
                 level digital platform that provides a turnkey solution to
                 online retailing.
               </h2>
-              <button className={"button common-btn mx-auto my-2 my-lg-5"}>
+              <button
+                className={"button common-btn mx-lg-0 mx-auto my-2 my-lg-5"}
+              >
                 Request Demo ›
               </button>
             </div>
           </div>
-          <div className={"col-12 col-md-10 col-lg-6 p-40 border-left"}>
+          <div
+            className={
+              "col-12 col-md-10 col-lg-6 p-40 border-left order-1 order-lg-2"
+            }
+          >
             <div className={"wrapperCol justify-content-center"}>
-              <img className={"bg-img rotation-animation"} src={glowImage} alt="glow"/>
-              <img src={sonosImage} className={"center-img bonos"} alt="bonos"/>
+              <img
+                className={"bg-img rotation-animation"}
+                src={glowImage}
+                alt="glow"
+              />
+              <img
+                src={sonosImage}
+                className={"center-img bonos"}
+                alt="bonos"
+              />
             </div>
           </div>
         </div>
